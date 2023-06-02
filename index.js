bf863ae0d21f68bfe96032ff46d1f890333274f2
Sincristina <77736213+Sincristina@users.noreply.github.com>
2023-06-02 13:05:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const toggleButton = document.getElementById("toggle-button");
+const naviList = document.getElementById("navi-list");
+
+toggleButton.addEventListener("click", () => {
+    naviList.classList.toggle("active");
+})
+
+
+
+/************ IMAGE POP UP  *******************/
+
+
+var modal = document.getElementById("myModal");
+
+
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+
+var modal = document.getElementById("myModal");
+
+
+var img = document.getElementById("myImg2");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
